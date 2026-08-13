d24cddfac00dfe3485e0887e3aba84deeb242b10
entec <miraobikz@gmail.com>
2026-08-14 00:05:53 +0100

    commit on dashboard edit

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,24 +1,20 @@
-from django.shortcuts import render
-
-# Create your views here.
-
+from django.shortcuts import render,get_object_or_404
 from groups.models import Group
 from withdrawals.models import BankInfo
+from wallet.models import Wallet
+
 
 # Create your views here.
 def dashboard(request):
     bankinfo=BankInfo.objects.filter(user=request.user).first()
-
     wallet = request.user.wallet
-
-    group_count = Group.objects.filter(
-        members=request.user
-    ).count()
+    group_count = Group.objects.filter(members=request.user).count()
+    transactions = wallet.transactions.all()
 
     context = {
         "wallet": wallet,
         "group_count": group_count,
-        'bankinfo':bankinfo
+        'bankinfo':bankinfo,
+        'transactions':transactions
     }
     return render(request,'group/dashboard.html',context)
-
