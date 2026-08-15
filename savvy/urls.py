b31b2032aa1b6a5c90fb809c7ada63bd86fb7d5f
entec <miraobikz@gmail.com>
2026-08-15 12:45:35 +0100

    committing to main

--- savvy/urls.py
+++ savvy/urls.py
@@ -15,6 +15,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
 from django.urls import path,include
 from django.conf import settings
 from django.conf.urls.static import static
@@ -28,7 +29,38 @@ urlpatterns = [
     path('wallet/',include('wallet.urls')),
     path('withdrawals/',include('withdrawals.urls')),
     path('notifications/',include('notifications.urls')),
-    path('deposits/',include('deposits.urls'))
+    path('deposits/',include('deposits.urls')),
+    path(
+        "password-reset/",
+        auth_views.PasswordResetView.as_view(
+            template_name="accounts/password_reset.html"
+        ),
+        name="password_reset",
+    ),
+
+    path(
+        "password-reset/done/",
+        auth_views.PasswordResetDoneView.as_view(
+            template_name="accounts/password_reset_done.html"
+        ),
+        name="password_reset_done",
+    ),
+
+    path(
+        "password-reset-confirm/<uidb64>/<token>/",
+        auth_views.PasswordResetConfirmView.as_view(
+            template_name="accounts/password_reset_confirm.html"
+        ),
+        name="password_reset_confirm",
+    ),
+
+    path(
+        "password-reset-complete/",
+        auth_views.PasswordResetCompleteView.as_view(
+            template_name="accounts/password_reset_complete.html"
+        ),
+        name="password_reset_complete",
+    )
 ]
 
 if settings.DEBUG:
